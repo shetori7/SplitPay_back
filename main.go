41459d624981a83d1710183b24a3fd051ea02f8c
shetori <shetori4818@gmail.com>
2025-03-08 13:17:42 +0900

    db接続部分作成

--- main.go
+++ main.go
@@ -1,17 +1,49 @@
+// package main
+
+// import (
+// 	"net/http"
+
+// 	"github.com/gin-gonic/gin"
+// )
+
+//	func main() {
+//		engine := gin.Default()
+//		engine.GET("/", func(c *gin.Context) {
+//			c.JSON(http.StatusOK, gin.H{
+//				"message": "hello world",
+//			})
+//		})
+//		engine.Run(":8000")
+//	}
 package main
 
 import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
+type User struct {
+	// gorm.Modelをつけると、idとCreatedAtとUpdatedAtとDeletedAtが作られる
+	gorm.Model
+
+	Name     string
+	Age      int
+	IsActive bool
+}
+
 func main() {
-	engine := gin.Default()
-	engine.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "hello world",
-		})
-	})
-	engine.Run(":8000")
+	// dbを作成します
+	db := dbInit()
+
+	// dbをmigrateします
+	db.AutoMigrate(&User{})
+}
+
+func dbInit() *gorm.DB {
+	dsn := "root:root@tcp(127.0.0.1:3306)/sample_db?charset=utf8mb4&parseTime=true"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	return db
 }
