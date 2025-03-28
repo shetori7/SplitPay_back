3a0797b06437c99524eefd16d930512c23d426b3
shetori <shetori4818@gmail.com>
2025-03-28 11:27:39 +0900

    URL設定途中まで

--- internal/infrastructure/router.go
+++ internal/infrastructure/router.go
@@ -46,11 +46,35 @@ func Init() {
 		c.JSON(http.StatusOK, users)
 	})
 
+	//sample
 	e.GET("/group/:id", func(c *gin.Context) {
 		groupID := c.Param("id")
 		c.JSON(http.StatusOK, gin.H{
 			"message":  "Group ID received",
 			"group_id": groupID,
+		})
+	})
+
+	//TODO：リクエストの構造体はここで持ってるでいいのか？
+	type RequestBody struct {
+		GroupID string   `json:"group_id"`
+		Users   []string `json:"users"`
+	}
+
+	e.POST("/group/new", func(c *gin.Context) {
+		var reqBody RequestBody
+
+		if err := c.ShouldBindJSON(&reqBody); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "received array",
+			"GroupID": reqBody.GroupID,
+			"Users":   reqBody.Users,
 		})
 	})
 
