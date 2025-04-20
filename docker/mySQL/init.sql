f0b6418a0ce978f982858dee58595568cf23a6a2
Ryohei-47 <ryo-hei4799@outlook.jp>
2025-04-20 22:11:31 +0900

    テーブル定義修正

--- docker/mySQL/init.sql
+++ docker/mySQL/init.sql
@@ -18,7 +18,6 @@ CREATE TABLE IF NOT EXISTS wari_users (
     FOREIGN KEY (group_id) REFERENCES wari_groups(group_id),
     user_created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     user_updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-    wari_loans_from_user_id INT,
     FOREIGN KEY (wari_loans_from_user_id) REFERENCES wari_users(user_id)
     wari_loans_amount DECIMAL(10, 2) NOT NULL,
 );
@@ -39,9 +38,24 @@ CREATE TABLE IF NOT EXISTS wari_payments (
 CREATE TABLE IF NOT EXISTS wari_loans (
     loan_id INT AUTO_INCREMENT PRIMARY KEY,
     payment_id INT,
-    user_id INT,
+    group_id INT,
+    from_user_id INT,
+    to_user_id INT,
+    amount DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (group_id) REFERENCES wari_groups(group_id),
     FOREIGN KEY (payment_id) REFERENCES wari_payments(payment_id),
     FOREIGN KEY (user_id) REFERENCES wari_users(user_id),
+);
+
+-- 最終的な支払いのテーブル（誰がいくら払ったか、全方向に向きをもつ、正と負で向きを判断する）
+CREATE TABLE IF NOT EXISTS wari_final_payments (
+    final_payment_id INT AUTO_INCREMENT PRIMARY KEY,
+    group_id INT,
+    from_user_id INT,
+    to_user_id INT,
+    amount DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (group_id) REFERENCES wari_groups(group_id),
+    FOREIGN KEY (user_id) REFERENCES wari_users(user_id)
 );
 
 -- データの初期化
