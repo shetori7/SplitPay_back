b512570f0b6affd728c785450b9cae69b68bdb25
shetori <shetori4818@gmail.com>
2025-06-21 17:38:14 +0900

    プッシュ

--- internal/domain/warigroup.go
+++ internal/domain/warigroup.go
@@ -1,8 +1,9 @@
 package domain
 
 type Wari_group struct {
-	GroupId   int         `gorm:"primaryKey;autoIncrement" json:"group_id"`
-	GroupName string      `json:"group_name"`
-	GroupUuid string      `json:"group_uuid"`
-	Users     []Wari_user `gorm:"-"`
+	GroupId   int                `gorm:"primaryKey;autoIncrement" json:"group_id"`
+	GroupName string             `json:"group_name"`
+	GroupUuid string             `json:"group_uuid"`
+	Users     []Wari_user        `gorm:"-"`
+	Payments  []WariFinalPayment `gorm:"-"`
 }
