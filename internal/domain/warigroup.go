1ff01d03877f601e5ae29f6418f1f4af006f18cc
Ryohei-47 <ryo-hei4799@outlook.jp>
2025-06-21 15:08:36 +0900

    コンフリクト解消

--- internal/domain/warigroup.go
+++ internal/domain/warigroup.go
@@ -1,13 +1,6 @@
 package domain
 
 type Wari_group struct {
-<<<<<<< HEAD
-	GroupId   int         `gorm:"primaryKey;autoIncrement" json:"group_id"`
-	GroupName string      `json:"group_name"`
-	GroupUuid string      `json:"group_uuid"`
-	Users     []Wari_user `gorm:"-"`
-=======
 	GroupName string `json:"group_name"`
 	GroupUuid string `json:"group_uuid"`
->>>>>>> feature-20250621-conflict
 }
